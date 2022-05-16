1e682e759ccc48e1a1ce6cb9ffd5e282451278eb
weshy <josephwaweru96@gmail.com>
2022-05-16 21:52:53 +0300

    model defined

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Listings(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.IntegerField()
+    num_ofbedroom = models.IntegerField(blank=True, null=True)
+    num_bathroom = models.IntegerField()
+    square_footage =models.IntegerField(blank=True, null=True)
+    address = models.CharField(max_length=100)
+    # images
+    
+    def __str__(self):
+        return self.title
